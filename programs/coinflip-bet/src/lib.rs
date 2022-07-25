1e0d3e77d22abb18e67f20ba5699b07a612de182
Andrii Kapinos <andy@everlend.fi>
2022-07-25 16:27:14 +0300

    flipping the coin

--- programs/coinflip-bet/src/lib.rs
+++ programs/coinflip-bet/src/lib.rs
@@ -3,25 +3,30 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod coinflip_bet {
+mod coinflip_bet {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>, data: i64) -> Result<()> {
         let my_account = &mut ctx.accounts.my_account;
         my_account.data = data;
+        my_account.won = false;
         Ok(())
     }
 
     pub fn update(ctx: Context<Update>) -> Result<()> {
         let my_account = &mut ctx.accounts.my_account;
-        my_account.data = Clock::get()?.unix_timestamp;
+        let timestamp = Clock::get()?.unix_timestamp;
+        let roll = ((timestamp + 7789) * 997) % 100;
+        let won = roll < 49;
+        my_account.data = roll;
+        my_account.won = won;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, space = 8 + 8)]
+    #[account(init, payer = user, space = 8 + 8 + 1)]
     pub my_account: Account<'info, MyAccount>,
     #[account(mut)]
     pub user: Signer<'info>,
@@ -37,4 +42,5 @@ pub struct Update<'info> {
 #[account]
 pub struct MyAccount {
     pub data: i64,
+    pub won: bool,
 }
--- tests/coinflip-bet.ts
+++ tests/coinflip-bet.ts
@@ -1,19 +1,68 @@
 import * as anchor from "@project-serum/anchor";
+import * as assert from "assert";
 import { Program } from "@project-serum/anchor";
 import { CoinflipBet } from "../target/types/coinflip_bet";
+const { SystemProgram } = anchor.web3;
+
+let _myAccount;
 
 describe("coinflip-bet", () => {
+  // Use a local provider.
+  const provider = anchor.AnchorProvider.local();
+
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  anchor.setProvider(provider);
 
-  const program = anchor.workspace.CoinflipBet as Program<CoinflipBet>;
+  it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
+    // #region code-simplified
+    // The program to execute.
+    const program = anchor.workspace.CoinflipBet;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const data = new anchor.BN(1);
-    const tx = await program.methods.initialize(data).rpc();
-    const two = await program.methods.update().rpc();
-    console.log("Your transaction signature", tx);
-    console.log("Your two signature", two);
+    // The Account to create.
+    const myAccount = anchor.web3.Keypair.generate();
+
+    // Create the new account and initialize it with the program.
+    // #region code-simplified
+    await program.rpc.initialize(new anchor.BN(1234), {
+      accounts: {
+        myAccount: myAccount.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [myAccount],
+    });
+    // #endregion code-simplified
+
+    // Fetch the newly created account from the cluster.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    // Check it's state was initialized.
+    assert.ok(account.data.eq(new anchor.BN(1234)));
+
+    // Store the account for the next test.
+    _myAccount = myAccount;
+  });
+
+  it("Updates a previously created account", async () => {
+    const myAccount = _myAccount;
+
+    // #region update-test
+
+    // The program to execute.
+    const program = anchor.workspace.CoinflipBet;
+
+    // Invoke the update rpc.
+    await program.rpc.update({
+      accounts: {
+        myAccount: myAccount.publicKey,
+      },
+    });
+
+    // Fetch the newly updated account.
+    const account = await program.account.myAccount.fetch(myAccount.publicKey);
+
+    console.log("account data");
+    console.log(account.data);
+    console.log(account.won);
   });
 });
