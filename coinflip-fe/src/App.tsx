e617ad2202543afa7de96c8d3840d80919073f4c
Andrii Kapinos <andy@everlend.fi>
2022-08-11 19:52:50 +0300

    removed comments

--- coinflip-fe/src/App.tsx
+++ coinflip-fe/src/App.tsx
@@ -14,22 +14,12 @@ export const App: FC = () => {
 };
 
 const Context: FC<{ children: ReactNode }> = ({ children }) => {
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
     const network = WalletAdapterNetwork.Devnet;
 
-    // You can also provide a custom RPC endpoint.
     const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
     const wallets = useMemo(
         () => [
-            /**
-             * Select the wallets you wish to support, by instantiating wallet adapters here.
-             *
-             * Common adapters can be found in the npm package `@solana/wallet-adapter-wallets`.
-             * That package supports tree shaking and lazy loading -- only the wallets you import
-             * will be compiled into your application, and only the dependencies of wallets that
-             * your users connect to will be loaded.
-             */
             new PhantomWalletAdapter(),
         ],
         []
