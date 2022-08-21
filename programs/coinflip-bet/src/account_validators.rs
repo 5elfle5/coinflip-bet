5223460d796bde705acb4b09aa1a0117837f9e13
Andrii Kapinos <broxbeats@gmail.com>
2022-08-21 17:53:47 +0300

    bet and flip smart contracts

--- programs/coinflip-bet/src/account_validators.rs
+++ programs/coinflip-bet/src/account_validators.rs
@@ -1,9 +1,9 @@
 use crate::*;
 use std::str::FromStr;
 
-impl<'info> Validate<'info> for Initialize<'info> {
+impl<'info> Validate<'info> for InitSystem<'info> {
     fn validate(&self) -> Result<()> {
-        assert_keys_eq!(self.user, Pubkey::from_str("DNNhz7iHfVYdjmJptwLmsA3LwocqnYYhwJn1j9LmaRXA").unwrap());
+        assert_keys_eq!(self.manager, Pubkey::from_str("DNNhz7iHfVYdjmJptwLmsA3LwocqnYYhwJn1j9LmaRXA").unwrap());
         Ok(())
     }
 }
--- programs/coinflip-bet/src/lib.rs
+++ programs/coinflip-bet/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*};
 use vipers::prelude::*;
 
 mod account_validators;
@@ -16,23 +16,56 @@ mod coinflip_bet {
     use super::*;
 
     #[access_control(ctx.accounts.validate())]
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn init_system(ctx: Context<InitSystem>) -> Result<()> {
         Ok(())
     }
 
-    pub fn update(ctx: Context<Update>) -> Result<()> {
-        let my_account = &mut ctx.accounts.flip_result;
+    pub fn bet(ctx: Context<Bet>, side: String) -> Result<()> {
+        let result = &mut ctx.accounts.flip_result;
+        result.roll = -1;
+        result.won = false;
+        result.bet_on_side = if side.eq_ignore_ascii_case("heads") { 1 } else { 0 };
+        Ok(())
+    }
+
+    pub fn flip(ctx: Context<Flip>) -> Result<()> {
+        let result = &mut ctx.accounts.flip_result;
         let timestamp = Clock::get()?.unix_timestamp;
         let roll = ((timestamp + 7789) * 997) % 100;
         let won = roll < 49;
-        my_account.roll = roll;
-        my_account.won = won;
+        result.roll = roll;
+        result.won = won;
+        if won {
+            // TODO: fix the number
+            let amount = 1000000;
+            let from_account = ctx.accounts.system_account.to_account_info();
+            if **from_account.try_borrow_lamports()? < amount {
+                return Err(anchor_lang::error!(CustomError::InsufficientFundsForTransaction));
+            }
+            **from_account.try_borrow_mut_lamports()? -= amount;
+            **ctx.accounts.user.to_account_info().try_borrow_mut_lamports()? += amount;
+        }
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize<'info> {
+pub struct InitSystem<'info> {
+    #[account(
+        init,
+        payer = manager,
+        space = 8 + 1,
+        seeds = [b"system-account", manager.key().as_ref()],
+        bump
+    )]
+    pub system_account: Account<'info, Escrow>,
+    #[account(mut)]
+    pub manager: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Bet<'info> {
     #[account(
         init,
         payer = user,
@@ -40,14 +73,27 @@ pub struct Initialize<'info> {
         seeds = [b"wager", user.key().as_ref()],
         bump
     )]
-    pub wager: Account<'info, Wager>,
+    pub wager: Account<'info, Escrow>,
     #[account(mut)]
     pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 8 + 1 + 1,
+        seeds = [b"flip-result", user.key().as_ref()],
+        bump
+    )]
+    pub flip_result: Account<'info, FlipResult>,
     pub system_program: Program<'info, System>,
 }
 
+
 #[derive(Accounts)]
-pub struct Update<'info> {
+pub struct Flip<'info> {
+    #[account(mut)]
+    pub system_account: Account<'info, Escrow>,
+    #[account(mut)]
+    pub user: Signer<'info>,
     #[account(mut)]
     pub flip_result: Account<'info, FlipResult>,
 }
@@ -56,9 +102,10 @@ pub struct Update<'info> {
 pub struct FlipResult {
     pub roll: i64,
     pub won: bool,
+    pub bet_on_side: u8,
 }
 
 #[account]
-pub struct Wager {
+pub struct Escrow {
     pub bump: u8,
 }
