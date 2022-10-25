adf887e60be042e42e0485170b2827105950d885
Andrii Kapinos <andy@everlend.fi>
2022-10-25 13:25:11 +0300

    bet test part 1

--- tests/coinflip-bet.ts
+++ tests/coinflip-bet.ts
@@ -1,19 +1,39 @@
-import * as anchor from "@project-serum/anchor";
-import { Keypair, PublicKey } from '@solana/web3.js';
-import * as assert from "assert";
-const { SystemProgram } = anchor.web3;
+import { AnchorProvider, BN, setProvider, workspace } from '@project-serum/anchor';
+import { Keypair, PublicKey, SystemProgram } from '@solana/web3.js';
 
 require('dotenv').config();
 const MANAGER = process.env.MANAGER ?? '';
 
 describe("coinflip-bet", () => {
-  const provider = anchor.AnchorProvider.local();
-  anchor.setProvider(provider);
+  const provider = AnchorProvider.local();
+  setProvider(provider);
 
-  it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
-    const program = anchor.workspace.CoinflipBet;
+  it("initiates system account", async () => {
+    // const program = workspace.CoinflipBet;
+    // const managerKeys = Keypair.fromSecretKey(new Uint8Array(JSON.parse(MANAGER)));
+    // const [systemAccount,] = await PublicKey.findProgramAddress(
+    //   [Buffer.from("system-account"), managerKeys.publicKey.toBuffer()],
+    //   program.programId
+    // );
+    // await program.rpc.initSystem({
+    //   accounts: {
+    //     systemAccount,
+    //     manager: managerKeys.publicKey,
+    //     systemProgram: SystemProgram.programId,
+    //   },
+    //   signers: [managerKeys],
+    // });
+
+    // const account = await program.account.flipResult.fetch(flipResult.publicKey);
+    // assert.ok(account.roll.eq(new BN(1234)));
+    // _flipResult = flipResult;
+  });
+
+  it("runs bet", async () => {
+    const program = workspace.CoinflipBet;
+    const userKeys = Keypair.generate();
+    // const managerPk = new PublicKey('Wj2yvieAgeD4bXdAzkVTYiqDgA8ScJM2JPpayDaCfcx');
     const managerKeys = Keypair.fromSecretKey(new Uint8Array(JSON.parse(MANAGER)));
-
     const [systemAccount,] = await PublicKey.findProgramAddress(
       [Buffer.from("system-account"), managerKeys.publicKey.toBuffer()],
       program.programId
@@ -26,22 +46,33 @@ describe("coinflip-bet", () => {
       },
       signers: [managerKeys],
     });
+    const [wager,] = await PublicKey.findProgramAddress(
+      [Buffer.from('wager'), userKeys.publicKey.toBuffer()],
+      program.programId,
+    );
+    const betArgs = {
+      side: 'heads',
+      lamports: new BN(1000),
+    }
+    await program.rpc.bet(betArgs, {
+      accounts: {
+        wager,
+        user: userKeys.publicKey,
+        systemAccount,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [userKeys]
+    });
+    // const flipResult = _flipResult;
+    // const program = workspace.CoinflipBet;
+    // await program.rpc.update({
+    //   accounts: {
+    //     flipResult: flipResult.publicKey,
+    //   },
+    // });
     // const account = await program.account.flipResult.fetch(flipResult.publicKey);
-    // assert.ok(account.roll.eq(new anchor.BN(1234)));
-    // _flipResult = flipResult;
-  });
-
-  it("Updates a previously created account", async () => {
-  //   const flipResult = _flipResult;
-  //   const program = anchor.workspace.CoinflipBet;
-  //   await program.rpc.update({
-  //     accounts: {
-  //       flipResult: flipResult.publicKey,
-  //     },
-  //   });
-  //   const account = await program.account.flipResult.fetch(flipResult.publicKey);
-  //   console.log("account data");
-  //   console.log(account.roll);
-  //   console.log(account.won);
+    // console.log("account data");
+    // console.log(account.roll);
+    // console.log(account.won);
   });
 });
