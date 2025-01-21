0d89c3a74e8ba4fe355c1ea0726ce236230e3a2f
Andrii Kapinos <broxbeats@gmail.com>
2025-01-21 18:18:32 +0200

    moved structs to a separate file

--- anchor/programs/coinflipbet/src/lib.rs
+++ anchor/programs/coinflipbet/src/lib.rs
@@ -2,6 +2,9 @@
 
 use anchor_lang::prelude::*;
 
+mod structs;
+use structs::*;
+
 declare_id!("coUnmi3oBUtwtd9fjeAvSsJssXh5A5xyPbhpewyzRVF");
 
 #[program]
@@ -37,85 +40,4 @@ pub mod coinflipbet {
     ctx.accounts.wager.count = value.clone();
     Ok(())
   }
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct FlipResult {
-  pub roll: i64,
-  pub won: bool,
-  pub bet_on_side: u8,
-}
-
-#[derive(Accounts)]
-pub struct CreateWager<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + 1 + 1 + 1 + 8,
-  payer = payer,
-  seeds = [b"wager", payer.key().as_ref()],
-  bump
-  )]
-  pub wager: Account<'info, Wager>,
-
-  pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct InitializeBankroll<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  init,
-  space = 8 + 8,
-  payer = payer,
-  seeds = [b"bankroll", payer.key().as_ref()],
-  bump
-  )]
-  pub bankroll: Account<'info, Bankroll>,
-
-  pub system_program: Program<'info, System>,
-}
-
-#[derive(Accounts)]
-pub struct CloseCoinflipbet<'info> {
-  #[account(mut)]
-  pub payer: Signer<'info>,
-
-  #[account(
-  mut,
-  close = payer,
-  )]
-  pub wager: Account<'info, Wager>,
-}
-
-#[derive(Accounts)]
-pub struct Update<'info> {
-  #[account(mut)]
-  pub wager: Account<'info, Wager>,
-}
-
-#[derive(Accounts)]
-pub struct UpdateBankroll<'info> {
-  #[account(mut)]
-  pub bankroll: Account<'info, Bankroll>,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Wager {
-  pub roll: u32,
-  pub won: bool,
-  pub bet_on_side: u8,
-  pub count: u8,
-}
-
-#[account]
-#[derive(InitSpace)]
-pub struct Bankroll {
-  pub count: u8,
 }
---
+++ anchor/programs/coinflipbet/src/structs.rs
@@ -0,0 +1,82 @@
+use anchor_lang::prelude::*;
+
+#[account]
+#[derive(InitSpace)]
+pub struct FlipResult {
+  pub roll: i64,
+  pub won: bool,
+  pub bet_on_side: u8,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Wager {
+  pub roll: u32,
+  pub won: bool,
+  pub bet_on_side: u8,
+  pub count: u8,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Bankroll {
+  pub count: u8,
+}
+
+#[derive(Accounts)]
+pub struct CreateWager<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+
+  #[account(
+  init,
+  space = 8 + 1 + 1 + 1 + 8,
+  payer = payer,
+  seeds = [b"wager", payer.key().as_ref()],
+  bump
+  )]
+  pub wager: Account<'info, Wager>,
+
+  pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeBankroll<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+
+  #[account(
+  init,
+  space = 8 + 8,
+  payer = payer,
+  seeds = [b"bankroll", payer.key().as_ref()],
+  bump
+  )]
+  pub bankroll: Account<'info, Bankroll>,
+
+  pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct CloseCoinflipbet<'info> {
+  #[account(mut)]
+  pub payer: Signer<'info>,
+
+  #[account(
+  mut,
+  close = payer,
+  )]
+  pub wager: Account<'info, Wager>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+  #[account(mut)]
+  pub wager: Account<'info, Wager>,
+}
+
+#[derive(Accounts)]
+pub struct UpdateBankroll<'info> {
+  #[account(mut)]
+  pub bankroll: Account<'info, Bankroll>,
+}
