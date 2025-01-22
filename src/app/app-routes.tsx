bf920cd227d3597fa05ed7dd3c0c44ef060aa770
Andrii Kapinos <broxbeats@gmail.com>
2025-01-22 18:50:00 +0200

    removed clusters and dashboard pages

--- src/app/app-routes.tsx
+++ src/app/app-routes.tsx
@@ -4,13 +4,10 @@ import { Navigate, RouteObject, useRoutes } from 'react-router-dom'
 
 const AccountListFeature = lazy(() => import('../components/account/account-list-feature'))
 const AccountDetailFeature = lazy(() => import('../components/account/account-detail-feature'))
-const ClusterFeature = lazy(() => import('../components/cluster/cluster-feature'))
 const CoinflipbetFeature = lazy(() => import('../components/coinflipbet/coinflipbet-feature'))
-const DashboardFeature = lazy(() => import('../components/dashboard/dashboard-feature'))
 
 const links: { label: string; path: string }[] = [
   { label: 'Account', path: '/account' },
-  { label: 'Clusters', path: '/clusters' },
   { label: 'Coinflipbet Program', path: '/coinflipbet' },
 ]
 
@@ -18,15 +15,12 @@ const routes: RouteObject[] = [
   { path: '/account/', element: <AccountListFeature /> },
   { path: '/account/:address', element: <AccountDetailFeature /> },
   { path: '/coinflipbet', element: <CoinflipbetFeature /> },
-  { path: '/clusters', element: <ClusterFeature /> },
 ]
 
 export function AppRoutes() {
   const router = useRoutes([
-    { index: true, element: <Navigate to={'/dashboard'} replace={true} /> },
-    { path: '/dashboard', element: <DashboardFeature /> },
+    { index: true, element: <Navigate to={'/account'} replace={true} /> },
     ...routes,
-    { path: '*', element: <Navigate to={'/dashboard'} replace={true} /> },
   ])
   return <UiLayout links={links}>{router}</UiLayout>
 }
